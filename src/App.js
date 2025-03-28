737186641c425c48e8e038e31be0c9c57e9eace0
Dania <152633361+daniasag@users.noreply.github.com>
2025-03-28 16:21:29 +0000

    final changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+// import CSS
+import 'bulma/css/bulma.css'
+
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
+
+// Importamos las imagenes
+import WandaImage from './imágenes/wanda.png'
+import LokiImage from './imágenes/loki.png'
+import NatImage from './imágenes/black.png'
+
 
 export function App() {
   return (
     <div>
-        <div>Personajes de Mario Bros</div>
-        <Tarjeta/>
-        <Tarjeta/>
-        <Tarjeta/>
+      <div style={{ fontSize: '35px', fontWeight: 'bold'}}> Personajes de Marvel </div>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-2">
+                <Tarjeta nombre="Scarlet Witch" colorFavorito="Rojo" image={WandaImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Loki Odinson" colorFavorito="Verde" image={LokiImage}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Black Widow" colorFavorito="Negro" image={NatImage}/>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
+  
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export function Tarjeta() {
+export function Tarjeta(props) {  // se le puede poner otro nombre
   return (
-    <div>
-        Tarjeta de Personaje
-    </div>
+    <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.image} alt="pda logo" />
+          </figure>
+        </div>
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{props.nombre}</p>
+            <p className="subtitle is-6">{props.colorFavorito} es mi color favorito</p>
+          </div>
+        </div>
+      </div>
+  
   )
 }
